89200bf1e4566961f71575f52077e3dad0bc03f0
Surjith <mahindhiran005@gmail.com>
2025-07-13 19:08:58 +0530

    extracting queryparams via observable

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,9 +1,19 @@
-import { Component, computed, inject, input } from '@angular/core';
+import {
+  Component,
+  computed,
+  DestroyRef,
+  inject,
+  input,
+  OnDestroy,
+  OnInit,
+  signal,
+  Signal,
+} from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
 import { TasksService } from './tasks.service';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tasks',
@@ -12,11 +22,31 @@ import { RouterLink } from '@angular/router';
   styleUrl: './tasks.component.css',
   imports: [TaskComponent, RouterLink],
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit, OnDestroy {
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
   userId = input.required<string>();
-  order = input<'asc' | 'desc'>();
+  // order = input<'asc' | 'desc'>();
+  order = signal<'asc' | 'desc'>('asc');
+  private destoryRef = inject(DestroyRef);
   private tasksService = inject(TasksService);
   userTasks: () => Task[] = computed(() =>
-    this.tasksService.allTasks().filter((task) => task.userId === this.userId())
+    this.tasksService
+      .allTasks()
+      .filter((task) => task.userId === this.userId())
+      .sort((a, b) =>
+        this.order() === 'asc'
+          ? a.title.localeCompare(b.title)
+          : b.title.localeCompare(a.title)
+      )
   );
+  private activatedRoute = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    const sub = this.activatedRoute.queryParams.subscribe({
+      next: (params) => this.order.set(params['order']),
+    });
+    this.destoryRef.onDestroy(() => sub.unsubscribe());
+  }
 }
