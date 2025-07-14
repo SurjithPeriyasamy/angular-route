9189666f215a37b66207812f59f0470e9084aaa7
Surjith <mahindhiran005@gmail.com>
2025-07-14 09:20:42 +0530

    seperate the children routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,14 @@
-import {
-  CanMatchFn,
-  RedirectCommand,
-  ResolveFn,
-  Router,
-  Routes,
-} from '@angular/router';
+import { CanMatchFn, RedirectCommand, Router, Routes } from '@angular/router';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import {
   resolveTitle,
   resolveUserName,
   UserTasksComponent,
 } from './users/user-tasks/user-tasks.component';
-import {
-  canLeaveEditPage,
-  NewTaskComponent,
-} from './tasks/new-task/new-task.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { inject } from '@angular/core';
-import { Task } from './tasks/task/task.model';
-import { TasksService } from './tasks/tasks.service';
+
+import { routes as usersRoutes } from './users/users.routes';
 
 export const dummyCanMatch: CanMatchFn = (routeData) => {
   console.log(routeData, 'routeData');
@@ -29,26 +19,6 @@ export const dummyCanMatch: CanMatchFn = (routeData) => {
     return true;
   }
   return new RedirectCommand(router.parseUrl('/unauthorized'));
-};
-const resolveUserTasks: ResolveFn<Task[]> = (
-  activatedRouteSnapshot,
-  routerState
-) => {
-  const order = activatedRouteSnapshot.queryParams['order'];
-  const tasksService = inject(TasksService);
-  const tasks = tasksService
-    .allTasks()
-    .filter(
-      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
-    );
-
-  if (order && order === 'asc') {
-    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
-  } else {
-    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
-  }
-
-  return tasks.length ? tasks : [];
 };
 
 export const routes: Routes = [
@@ -64,27 +34,7 @@ export const routes: Routes = [
       userName: resolveUserName,
     },
     title: resolveTitle,
-    children: [
-      {
-        path: '',
-        redirectTo: 'tasks',
-        pathMatch: 'full',
-      },
-      {
-        path: 'tasks',
-        loadComponent: () =>
-          import('./tasks/tasks.component').then((mod) => mod.TasksComponent),
-        runGuardsAndResolvers: 'always',
-        resolve: {
-          userTasks: resolveUserTasks,
-        },
-      },
-      {
-        path: 'tasks/new',
-        component: NewTaskComponent,
-        canDeactivate: [canLeaveEditPage],
-      },
-    ],
+    children: usersRoutes,
   },
   {
     path: '**',
---
+++ src/app/users/users.routes.ts
@@ -0,0 +1,50 @@
+import { ResolveFn, Routes } from '@angular/router';
+import { TasksService } from '../tasks/tasks.service';
+import { inject } from '@angular/core';
+import { Task } from '../tasks/task/task.model';
+import {
+  canLeaveEditPage,
+  NewTaskComponent,
+} from '../tasks/new-task/new-task.component';
+
+const resolveUserTasks: ResolveFn<Task[]> = (
+  activatedRouteSnapshot,
+  routerState
+) => {
+  const order = activatedRouteSnapshot.queryParams['order'];
+  const tasksService = inject(TasksService);
+  const tasks = tasksService
+    .allTasks()
+    .filter(
+      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
+    );
+
+  if (order && order === 'asc') {
+    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
+  } else {
+    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
+  }
+
+  return tasks.length ? tasks : [];
+};
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'full',
+  },
+  {
+    path: 'tasks',
+    loadComponent: () =>
+      import('../tasks/tasks.component').then((mod) => mod.TasksComponent),
+    runGuardsAndResolvers: 'always',
+    resolve: {
+      userTasks: resolveUserTasks,
+    },
+  },
+  {
+    path: 'tasks/new',
+    component: NewTaskComponent,
+    canDeactivate: [canLeaveEditPage],
+  },
+];
